099bda4732568c59afe17088c4579f4538f7e5fa
hight0werdodgev <hight0werdodgev@gmail.com>
2021-02-06 14:06:13 +0300

    refactor, trans en-ru

--- modules/admin/views/article/_form.php
+++ modules/admin/views/article/_form.php
@@ -14,22 +14,14 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'meta_description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
-
-    <?= $form->field($model, 'date_of_creation')->textInput() ?>
-
-    <?= $form->field($model, 'date_of_change')->textInput() ?>
-
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
