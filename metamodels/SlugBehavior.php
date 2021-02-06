32002e237fe5daf60e826cdadbdcbb0ad9bdce4b
hight0werdodgev <hight0werdodgev@gmail.com>
2021-02-06 13:31:02 +0300

    SlugBehavior model +

---
+++ metamodels/SlugBehavior.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace app\metamodels;
+
+
+trait SlugBehavior
+{
+    public function behaviors()
+    {
+        return [
+            'slug' => [
+                'class' => 'Zelenin\yii\behaviors\Slug',
+                'slugAttribute' => 'slug',
+                'attribute' => 'title',
+                // optional params
+                'ensureUnique' => true,
+                'replacement' => '-',
+                'lowercase' => true,
+                'immutable' => false,
+                // If intl extension is enabled, see http://userguide.icu-project.org/transforms/general.
+                'transliterateOptions' => 'Russian-Latin/BGN; Any-Latin; Latin-ASCII; NFD; [:Nonspacing Mark:] Remove; NFC;'
+            ]
+        ];
+    }
+
+}
