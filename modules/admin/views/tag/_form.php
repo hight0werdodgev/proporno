16082b7e75d70d9509e033c0b10bc41f87fe2be4
hight0werdodgev <hight0werdodgev@gmail.com>
2021-02-08 13:02:00 +0300

    slug fields -

--- modules/admin/views/tag/_form.php
+++ modules/admin/views/tag/_form.php
@@ -14,8 +14,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
-
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
